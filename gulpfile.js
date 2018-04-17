73bcbc2d5e5e908a89d713850c037f9b6f0466e6
Tyler Mutch <tyler@mutch.co>
2018-04-17 13:39:15 -0600

    Change property list format

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ const jsdoc2md = require('jsdoc-to-markdown');
 const fs = require('fs-then-native');
 
 gulp.task('default', function () {
-  return jsdoc2md.render({ files: 'src/**/*.js' })
+  return jsdoc2md.render({ files: 'src/**/*.js', 'param-list-format': 'list' })
   .then(output => fs.writeFile('docs/api.md', output));
 });
